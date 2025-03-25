d3584be0887edfebef7cfa704577d81ae0210ddd
TshiamoTodd <tshiamo.mokwena19@gmail.com>
2025-03-25 13:10:04 +0200

    Protected routes complete

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,14 @@
+import { isAuthenticated } from '@/lib/auth.action'
+import { redirect } from 'next/navigation'
 import React, { ReactNode } from 'react'
 
-const AuthLayout = ({children}: {children: ReactNode}) => {
+const AuthLayout = async ({children}: {children: ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated()
+  console.log(isUserAuthenticated)
+    
+    if(isUserAuthenticated) {
+      redirect('/')
+    }
   return (
     <div className='auth-layout'>
         {children}
